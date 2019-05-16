621ccdd0b83148f4110a05e434da94004c720ae7
Sam Harlan <sams.email24@gmail.com>
2019-05-16 09:44:23 -0700

    working isbn checker
    isbn works for 10 and 13 digit isbn's. changed index styling slightly.

--- checkISBN.js
+++ checkISBN.js
@@ -6,45 +6,95 @@ app.use(express.static('public'));
 app.get('/checkisbn', function handleProcessGet(request, response) {
   // length must be 10
       var isbn = request.query.isbn;
-      var n = isbn.length();
-      if (n != 10)
+
+      //remove any hyphens
+      isbn = isbn.replace(/-/g, "");
+      isbn = isbn.replace(/ /g, "");
+
+      var isbn_length = isbn.toString().length;
+
+      if (isbn_length == 10)
       {
-          response.send("Bad ISBN");
+        // Computing weighted sum
+        // of first 9 digits
+        var sum = 0;
+        for (var i = 0; i < 9; i++)
+        {
+            var digit = isbn[i] - '0';
+            if (0 > digit || 9 < digit)
+            {
+                response.write("Bad ISBN\n");
+                response.write("10 Digit isbn\n");
+                response.write("Contains Inpropper Characters");
+                response.end();
+            }
+            sum += (digit * (10 - i));
+        }
+
+        // Checking last digit.
+        var last = isbn[9];
+        if (last != 'X' && (last < '0' || last > '9'))
+        {
+          response.write("Bad ISBN\n");
+          response.write("10 Digit isbn\n");
+          response.write("Bad Final Character");
+          response.end();
+        }
+
+        // If last digit is 'X', add 10
+        // to sum, else add its value.
+        sum += ((last == 'X') ? 10 : (last - '0'));
+
+        // Return true if weighted sum
+        // of digits is divisible by 11.
+        if(sum % 11 == 0)
+        {
+          response.write("Good ISBN\n");
+          response.write("10 Digit isbn");
+          response.end();
+        }
+        else
+        {
+          response.write("Bad ISBN\n");
+          response.write("10 Digit isbn\n");
+          response.write("Bad Checksum");
+          response.end();
+        }
       }
 
-      // Computing weighted sum
-      // of first 9 digits
-      var sum = 0;
-      for (var i = 0; i < 9; i++)
-      {
-          var digit = isbn[i] - '0';
-          if (0 > digit || 9 < digit)
-          {
-              response.send("Bad ISBN");
-          }
-          sum += (digit * (10 - i));
-      }
+      // 13 digit isbn number
+      else if (isbn_length == 13) {
 
-      // Checking last digit.
-      var last = isbn[9];
-      if (last != 'X' && (last < '0' || last > '9'))
-      {
-          response.send("Bad ISBN");
-      }
+        var tot = 0;
+        for ( var i = 0; i < 12; i++ )
+        {
+             var digit = Number( isbn.substring( i, i + 1 ) );
+             tot += (i % 2 == 0) ? digit * 1 : digit * 3;
+        }
 
-      // If last digit is 'X', add 10
-      // to sum, else add its value.
-      sum += ((last == 'X') ? 10 : (last - '0'));
-
-      // Return true if weighted sum
-      // of digits is divisible by 11.
-      if(sum % 11 == 0)
-      {
-        response.send("Good ISBN");
+        //checksum must be 0-9. If calculated as 10 then = 0
+        var checksum = 10 - (tot % 10);
+        if ( checksum == 10 )
+        {
+            checksum = 0;
+        }
+        if (checksum == Number(isbn.substring( 12 ))) {
+          response.write("Good ISBN\n");
+          response.write("13 Digit isbn");
+          response.end();
+        }
+        else {
+          response.write("Bad ISBN\n");
+          response.write("13 Digit isbn\n");
+          response.write("bad Checksum");
+          response.end();
+        }
       }
-      else
-      {
-        response.send("Bad ISBN");
+      // isbn length is bad
+      else {
+        response.write("Bad ISBN \n");
+        response.write("Inpropper Length");
+        response.end();
       }
 });
 
